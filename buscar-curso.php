a497382b72499f9d12e6a80569efae4b4846494e
tunino <tuninoff@gmail.com>
2024-03-12 16:39:20 -0300

    primeiro commit

---
+++ buscar-curso.php
@@ -0,0 +1,22 @@
+<?php 
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client([
+    'base_uri' => 'https://www.alura.com.br/',
+    'verify' => false
+]);
+$response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $response->getBody();
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
